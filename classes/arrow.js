e76acd8e9b0563252671dafa900ef3cc8d809d23
ranjansinghkol <38905611+ranjansinghkol@users.noreply.github.com>
2021-10-15 18:48:09 +0530

    Update arrow.js

--- classes/arrow.js
+++ classes/arrow.js
@@ -1,14 +1,15 @@
 class Arrow {
-    constructor(x, y) {
+    constructor(x, y, width, height, archerAngle) {
         var options = {
-            isStatic: true
+            isStatic: true,
+            density: 0.1
         };
-
-        this.width = 50;
-        this.height = 25;
-        this.image = loadImage("assets/arrow.png");
-        this.body = Bodies.rectangle(x, y, 50, 25, options);
-
+        this.width = width;
+        this.height = height;
+        this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+        this.image = loadImage("./assets/arrow.png");
+        this.archerAngle = archerAngle;
+        this.velocity = 0;
         World.add(world, this.body);
     }
 
@@ -28,12 +29,24 @@ class Arrow {
     }
 
     display() {
+        var tmpAngle;
+
+        if (this.body.velocity.y == 0) {
+            tmpAngle = this.archerAngle + 90;
+        } else {
+            tmpAngle = Math.atan(this.body.velocity.y / this.body.velocity.x) * (180 / 3.14);
+        }
+
+        Matter.Body.setAngle(this.body, tmpAngle);
+
         var pos = this.body.position;
-        
+        var angle = this.body.angle;
+
         push();
         translate(pos.x, pos.y);
+        rotate(angle);
         imageMode(CENTER);
-        image(this.image, pos.x, pos.y, this.width, this.height);
+        image(this.image, 0, 0, this.width, this.height);
         pop();
     }
 }
